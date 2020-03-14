c6bb94ac7d9c6d88f6a12714c952069b624d408e
phienvan@outlook.com <dangquocphongbni98@gmail.com>
2020-03-14 18:39:54 +0700

    add theme

--- Nothing.Web/Startup.cs
+++ Nothing.Web/Startup.cs
@@ -50,7 +50,7 @@ namespace Nothing.Web
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=TestTheme}/{action=Index}/{id?}");
             });
         }
     }
