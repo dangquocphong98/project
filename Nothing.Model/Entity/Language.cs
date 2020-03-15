edcaef760c569cd62e11061b50a96875e066fc81
phienvan@outlook.com <dangquocphongbni98@gmail.com>
2020-03-15 12:10:39 +0700

    add entity

---
+++ Nothing.Model/Entity/Language.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nothing.Model.Entity
+{
+    public class Language
+    {
+        public int ID_Language { get; set; }
+        public string Key_Language { get; set; }
+        public string Language_VN { get; set; }
+        public string Language_EN { get; set; }
+    }
+}
